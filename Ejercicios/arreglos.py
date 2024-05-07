1b9d78fc1125644354121b9e0ee3e1a04f0eed13
Sebastian <duartesebastian564@gmail.com>
2024-05-06 19:53:09 -0500

    Se a actualizado para una mejor comprension

--- Ejercicios/arreglos.py
+++ Ejercicios/arreglos.py
@@ -6,7 +6,9 @@ lista = [1.5, 2.0, 1.8]
 print(lista)
 print(lista[0])
 lista_1 = []
+# se utiliza None para crear las posiciones a utilizar
 lista_2 = [None] * 4
+print(lista_2)
 
 arreglo = []
 arreglo.append(10)
@@ -16,9 +18,36 @@ arreglo.append(30)
 print("Datos del arreglo")
 print(arreglo)
 
+#razas.append("galgo")
+#razas.append("podenco")
+#razas.append("criollo")
+#razas.append("chihuahua")
+#razas.append("bulldog")
+
+# Ingrese 4 razas de perro.
 razas = []
-razas.append("galgo")
-razas.append("podenco")
-razas.append("criollo")
-razas.append("chihuahua")
-razas.append("bulldog")
+for i in range (0,4):
+    r = input("Escribe una raza de perro")
+    razas.append(r)
+print(razas)
+
+#todas las razas de gato que recuerde el usuario.
+razas_G = []
+continuar = "s"
+while (continuar == "s") or (continuar == "S"):
+    g = input("Escribe una raza gatuna. ")
+    razas_G.append(g)
+    continuar = input("¿Desea continuar? s/n")    
+print(razas_G)
+
+#todas las razas de gato que recuerde el usuario si la varible continuar.
+razas_G = []
+while True:
+    g = input("Escribe una raza gatuna. ")
+    razas_G.append(g)
+    continuar = input("¿Desea continuar? s/n")   
+    if continuar != "s" or "S":
+        break
+print(razas_G)
+
+# variable.insert (posicion, texto a insertar)
