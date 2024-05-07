47784cee1d9d63726ba5f09b9cf3b3fea99a9b33
Sebastian <duartesebastian564@gmail.com>
2024-05-06 19:23:03 -0500

    Se hizo se realizo los respectivos cambios

--- Ejercicios/arreglos.py
+++ Ejercicios/arreglos.py
@@ -7,3 +7,18 @@ print(lista)
 print(lista[0])
 lista_1 = []
 lista_2 = [None] * 4
+
+arreglo = []
+arreglo.append(10)
+arreglo.append(20)
+arreglo.append(30)
+
+print("Datos del arreglo")
+print(arreglo)
+
+razas = []
+razas.append("galgo")
+razas.append("podenco")
+razas.append("criollo")
+razas.append("chihuahua")
+razas.append("bulldog")
