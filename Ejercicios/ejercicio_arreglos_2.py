c4a35abc0582791d5e8d8f29509d3691c11b5808
Sebastian <duartesebastian564@gmail.com>
2024-05-20 19:55:31 -0500

    Ejercicio 2

---
+++ Ejercicios/ejercicio_arreglos_2.py
@@ -0,0 +1,15 @@
+# Segundo ejercicio de Python con arreglos.
+# Autor: Sebastian Duarte Mera 2421811-2725
+# 20/05/2024 list.count
+
+lista = []
+confirm = "S"
+buscar = 0
+
+while (confirm == "S") or (confirm == "s"):
+    lista.append(int(input("Ingresa un número entero: ")))
+    confirm = input("Desea continuar? s/n ")
+    
+for i in 
+    lista.count(int(input("Ingrese el número que desea buscar: ")))
+
