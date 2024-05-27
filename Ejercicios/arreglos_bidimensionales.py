6f51b7cccced506ee98eb33ae96522f1f56fa74e
Sebastian <duartesebastian564@gmail.com>
2024-05-27 18:51:48 -0500

    Nuevo tema Arreglos Bidimencionales

---
+++ Ejercicios/arreglos_bidimensionales.py
@@ -0,0 +1,5 @@
+# variable = [[dato1, dato2,], [dato3, dato4]]
+nombres = [["Oscar", "Gomez",], ["Juan", "Perez"], ["Jhon", "Yepes"]]
+
+# Imprimir el primer elemento print(variable[fila][columna])
+print(nombres[2][1])
